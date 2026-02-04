a34e53c4589b628b56ebea0e46358285acc5cced
YTGLD <ytgld123123123@163.com>
2026-02-04 19:31:55 +0800

    混沌意识：降低迷雾出现的最小距离 恶念掠夺：击杀对你有敌意的生物会获得额外的掉落物奖励 恶意转化：降低受到的魔法和虚空伤害，并将受到的对应伤害转换成临时的攻击伤害

--- src/main/java/com/ytgld/chest_item/ChestitemClient.java
+++ src/main/java/com/ytgld/chest_item/ChestitemClient.java
@@ -1,13 +1,11 @@
 package com.ytgld.chest_item;
 
-import com.mojang.blaze3d.vertex.VertexConsumer;
 import com.ytgld.chest_item.event.Keys;
 import com.ytgld.chest_item.items.AttReg;
 import com.ytgld.chest_item.items.InitItems;
 import com.ytgld.chest_item.other.ChestMenuScreen;
 import com.ytgld.chest_item.other.ChestMenuTypes;
 import com.ytgld.chest_item.renderer.MRender;
-import com.ytgld.chest_item.renderer.model.IAvatarRenderState;
 import com.ytgld.chest_item.renderer.particle.ColorPart;
 import com.ytgld.chest_item.renderer.particle.IParticleEngine;
 import com.ytgld.chest_item.renderer.particle.other.Particles;
@@ -15,10 +13,7 @@ import net.minecraft.client.Minecraft;
 import net.minecraft.client.entity.ClientAvatarEntity;
 import net.minecraft.client.particle.ParticleGroup;
 import net.minecraft.client.particle.ParticleRenderType;
-import net.minecraft.client.renderer.LevelRenderer;
-import net.minecraft.client.renderer.rendertype.RenderType;
 import net.minecraft.world.entity.Avatar;
-import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.phys.Vec3;
 import net.neoforged.api.distmarker.Dist;
@@ -78,39 +73,13 @@ public class ChestitemClient{
         }
     }
     public static Queue<?> iterateParticles(Map<ParticleRenderType, ParticleGroup<?>> map) {
-        for (ParticleRenderType renderType : map.keySet()){
-            if (renderType != ParticleRenderType.NO_RENDER){
+        for (ParticleRenderType renderType : map.keySet()) {
+            if (renderType != ParticleRenderType.NO_RENDER) {
                 ParticleGroup<?> particleGroup = map.get(renderType);
                 return particleGroup.getAll();
             }
         }
         return null;
-    }
-    @SubscribeEvent
-    public static void AfterParticles(RenderLevelStageEvent.AfterParticles event){
-        if (Minecraft.getInstance().particleEngine instanceof IParticleEngine iParticleEngine) {
-            for (ParticleRenderType particlerendertype : iParticleEngine.cI1_21_11$particles().keySet()) {
-                if (particlerendertype != ParticleRenderType.NO_RENDER) {
-                    Queue<?> q = iterateParticles(iParticleEngine.cI1_21_11$particles());
-                    if (q != null) {
-                        for (Object o : q){
-                            if (o instanceof ColorPart colorPart) {
-                                var offset = colorPart.getPos().subtract(event.getLevelRenderState().cameraRenderState.pos);
-                                event.getPoseStack().pushPose();
-                                RenderType renderType = MRender.colorOutline(false);
-                                VertexConsumer consumer = Minecraft.getInstance().renderBuffers().bufferSource().getBuffer(renderType);
-                                event.getPoseStack().translate(offset.x, offset.y, offset.z);
-
-                                colorPart.setT(event.getPoseStack(),colorPart, consumer);
-
-                                Minecraft.getInstance().renderBuffers().bufferSource().endBatch(renderType);
-                                event.getPoseStack().popPose();
-                            }
-                        }
-                    }
-                }
-            }
-        }
     }
 
     @SubscribeEvent
