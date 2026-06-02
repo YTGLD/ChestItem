55f6881678ce72890f3607fa3756b49784efbb2f
YTGLD <ytgld123123123@163.com>
2026-06-02 13:21:47 +0800

    一些优化

--- src/main/java/com/ytgld/chest_item/renderer/book/BookPageAll.java
+++ src/main/java/com/ytgld/chest_item/renderer/book/BookPageAll.java
@@ -1,6 +1,7 @@
 package com.ytgld.chest_item.renderer.book;
 
 import com.ytgld.chest_item.items.InitItems;
+import com.ytgld.chest_item.items.ItemBase;
 import com.ytgld.chest_item.renderer.book.tool.AddBookPage;
 import com.ytgld.chest_item.renderer.book.tool.RegisterBookPage;
 import com.ytgld.chest_item.renderer.light.Light;
@@ -14,49 +15,79 @@ public class BookPageAll {
     public static class Self_Increasing_Heart implements RegisterBookPage {
         @Override
         public void addPage(List<CIBookScreen.CIBookGuiAdd> list) {
-            list.add(new CIBookScreen.CIBookGuiAdd(InitItems.Self_Increasing_Heart.asItem(),new Vec2(32,-64),
-                    Component.translatable("chest_item.book.self_increasing_heart.main"),
-                    List.of(
-                            Component.translatable("chest_item.book.self_increasing_heart.1"),
-                            Component.translatable("chest_item.book.self_increasing_heart.2"),
-                            Component.translatable("chest_item.book.self_increasing_heart.3")
-                    ),
-                    Light.ARGB.color(255,255,255,255),
-                    Light.ARGB.color(255,150,150,150),
-                    CIBookScreen.ThePage.MEAT));
+            if (InitItems.Self_Increasing_Heart.asItem() instanceof ItemBase itemBase) {
+                list.add(new CIBookScreen.CIBookGuiAdd(InitItems.Self_Increasing_Heart.asItem(), new Vec2(32, -64),
+                        Component.translatable("chest_item.book.self_increasing_heart.main"),
+                        List.of(
+                                Component.translatable("chest_item.book.self_increasing_heart.1"),
+                                Component.translatable("chest_item.book.self_increasing_heart.2"),
+                                Component.translatable("chest_item.book.self_increasing_heart.3")
+                        ),
+                        Light.ARGB.color(255, 255, 255, 255),
+                        Light.ARGB.color(255, 150, 150, 150),
+                        CIBookScreen.ThePage.MEAT,
+                        itemBase.color(InitItems.Self_Increasing_Heart.asItem().getDefaultInstance())));
+            }
         }
     }
     @AddBookPage
     public static class Bone_Head implements RegisterBookPage {
         @Override
         public void addPage(List<CIBookScreen.CIBookGuiAdd> list) {
-            list.add(new CIBookScreen.CIBookGuiAdd(InitItems.Bone_Head.asItem(),new Vec2(0,32),
-                    Component.translatable("chest_item.book.bone_head.main"),
-                    List.of(
-                            Component.translatable("chest_item.book.bone_head.1"),
-                            Component.translatable("chest_item.book.bone_head.2"),
-                            Component.translatable("chest_item.book.bone_head.3"),
-                            Component.translatable("chest_item.book.bone_head.4")
-                    ),
-                    Light.ARGB.color(255,255,255,255),
-                    Light.ARGB.color(255,150,150,150),
-                    CIBookScreen.ThePage.BASE));
+            if (InitItems.Bone_Head.asItem() instanceof ItemBase itemBase) {
+                list.add(new CIBookScreen.CIBookGuiAdd(InitItems.Bone_Head.asItem(), new Vec2(0, 32),
+                        Component.translatable("chest_item.book.bone_head.main"),
+                        List.of(
+                                Component.translatable("chest_item.book.bone_head.1"),
+                                Component.translatable("chest_item.book.bone_head.2"),
+                                Component.translatable("chest_item.book.bone_head.3"),
+                                Component.translatable("chest_item.book.bone_head.4")
+                        ),
+                        Light.ARGB.color(255, 255, 255, 255),
+                        Light.ARGB.color(255, 150, 150, 150),
+                        CIBookScreen.ThePage.BASE,
+                        itemBase.color(InitItems.Bone_Head.asItem().getDefaultInstance())));
+            }
         }
     }
     @AddBookPage
     public static class Heart_ implements RegisterBookPage {
         @Override
         public void addPage(List<CIBookScreen.CIBookGuiAdd> list) {
-            list.add(new CIBookScreen.CIBookGuiAdd(InitItems.Heart_.asItem(),new Vec2(0,-32),
-                    Component.translatable("chest_item.book.heart.main"),
-                    List.of(
-                            Component.translatable("chest_item.book.heart.1"),
-                            Component.translatable("chest_item.book.heart.2"),
-                            Component.translatable("chest_item.book.heart.3")
-                    ),
-                    Light.ARGB.color(255,255,255,255),
-                    Light.ARGB.color(255,150,150,150),
-                    CIBookScreen.ThePage.MEAT));
+            if (InitItems.Heart_.asItem() instanceof ItemBase itemBase) {
+                list.add(new CIBookScreen.CIBookGuiAdd(InitItems.Heart_.asItem(), new Vec2(0, -32),
+                        Component.translatable("chest_item.book.heart.main"),
+                        List.of(
+                                Component.translatable("chest_item.book.heart.1"),
+                                Component.translatable("chest_item.book.heart.2"),
+                                Component.translatable("chest_item.book.heart.3")
+                        ),
+                        Light.ARGB.color(255, 255, 255, 255),
+                        Light.ARGB.color(255, 150, 150, 150),
+                        CIBookScreen.ThePage.MEAT,
+                        itemBase.color(InitItems.Heart_.asItem().getDefaultInstance())));
+            }
+        }
+    }
+
+    @AddBookPage
+    public static class FleshAndBloodGears_ implements RegisterBookPage {
+        @Override
+        public void addPage(List<CIBookScreen.CIBookGuiAdd> list) {
+            if (InitItems.Heart_.asItem() instanceof ItemBase itemBase) {
+                list.add(new CIBookScreen.CIBookGuiAdd(InitItems.FleshAndBloodGears_.asItem(), new Vec2(-32, -64),
+                        Component.translatable("chest_item.book.flesh_and_blood_gears.main"),
+                        List.of(
+                                Component.translatable("chest_item.book.flesh_and_blood_gears.1"),
+                                Component.translatable("chest_item.book.flesh_and_blood_gears.2"),
+                                Component.translatable("chest_item.book.flesh_and_blood_gears.3"),
+                                Component.translatable("chest_item.book.flesh_and_blood_gears.4")
+                        ),
+                        Light.ARGB.color(255, 255, 255, 255),
+                        Light.ARGB.color(255, 150, 150, 150),
+                        CIBookScreen.ThePage.MEAT,
+                        itemBase.color(InitItems.FleshAndBloodGears_.asItem().getDefaultInstance())));
+            }
         }
     }
 }
--- src/main/java/com/ytgld/chest_item/renderer/book/CIBookScreen.java
+++ src/main/java/com/ytgld/chest_item/renderer/book/CIBookScreen.java
@@ -2,11 +2,15 @@ package com.ytgld.chest_item.renderer.book;
 
 import com.ytgld.chest_item.Chestitem;
 import com.ytgld.chest_item.items.AttReg;
-import com.ytgld.chest_item.items.InitItems;
+import com.ytgld.chest_item.items.SkillItem;
+import com.ytgld.chest_item.renderer.MRender;
 import com.ytgld.chest_item.renderer.book.tool.AddBookPage;
 import com.ytgld.chest_item.renderer.book.tool.BookPageFinder;
 import com.ytgld.chest_item.renderer.book.tool.RegisterBookPage;
 import com.ytgld.chest_item.renderer.light.Light;
+import com.ytgld.chest_item.tip.an_element.SkillList;
+import com.ytgld.chest_item.tip.an_element.extend.BlackSkill;
+import com.ytgld.chest_item.tip.an_element.extend.SkillBase;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiGraphicsExtractor;
 import net.minecraft.client.gui.components.Button;
@@ -25,9 +29,11 @@ import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.phys.Vec2;
+import org.joml.Matrix3x2fStack;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 public class CIBookScreen extends Screen {
     private static final Identifier window =
@@ -44,6 +50,7 @@ public class CIBookScreen extends Screen {
     private double lastMouseX;
     private double lastMouseY;
 
+    private float size = 1;
     public CIBookScreen(Player player) {
         super(TITLE);
         this.player = player;
@@ -155,11 +162,64 @@ public class CIBookScreen extends Screen {
         ItemStack stack = new ItemStack(ciBookGuiAdd.item);
 
 
+        boolean big  = mouseX >= centerX - 8 && mouseX <= centerX + 8 &&
+                mouseY >= centerY - 8 && mouseY <= centerY + 8;
+
+        Matrix3x2fStack pose = graphics.pose();
+        //down == 1
+        float speed = 20;
+        float max = 5 / speed;
+        if (big) {
+            if (size < max * speed){
+                size += max;
+            }
+        }else {
+            if (size > 1) {
+                size -= max;
+            }
+        }
+
+        graphics.blit(RenderPipelines.GUI_TEXTURED,ciBookGuiAdd.thePage.identifier,centerX - 9, centerY - 9,0,0,18,18,18,18);
+        int color = ciBookGuiAdd.lightColor;
+        int as = (color >> 24) & 0xFF;
+        int rs = (color >> 16) & 0xFF;
+        int gs = (color >> 8) & 0xFF;
+        int bs = color & 0xFF;
 
 
-        graphics.item(stack, centerX - 8, centerY - 8);
+        int sss = 64;
+        pose.pushMatrix();
+        pose.translate(centerX, centerY);
+        pose.scale(size,size);
+        pose.translate( (float) -sss / 2,  (float) -sss / 2);
+        graphics.blit(MRender.RenderPs.GUI_TEXTURED, Identifier.fromNamespaceAndPath(Chestitem.MODID,
+                        "textures/item_glowing/all.png"),
+                0,0,
+                0, 0,
+                sss, sss, sss, sss,
+                Light.ARGB.color((int) (size * 400 - 400), rs, gs, bs));
+        graphics.blit(MRender.RenderPs.GUI_TEXTURED, Identifier.fromNamespaceAndPath(Chestitem.MODID,
+                        "textures/shadow/black_2.png"),
+                0,0,
+                0, 0,
+                sss, sss, sss, sss,
+                Light.ARGB.color((int) (size * 400 - 400), rs, gs, bs));
+        graphics.blit(MRender.RenderPs.GUI_TEXTURED, Identifier.fromNamespaceAndPath(Chestitem.MODID,
+                        "textures/shadow/black_3.png"),
+                0,0,
+                0, 0,
+                sss, sss, sss, sss,
+                Light.ARGB.color((int) (size * 400 - 400), rs, gs, bs));
+        pose.popMatrix();
 
-        graphics.blit(RenderPipelines.GUI_TEXTURED,ciBookGuiAdd.thePage.identifier,centerX - 8, centerY - 8,0,0,16,16,16,16);
+
+        pose.pushMatrix();
+        pose.translate(centerX, centerY);
+        pose.scale(size,size);
+        pose.translate( - 8,  - 8);
+        graphics.item(stack, 0,0);
+        pose.popMatrix();
+
         if (!has(stack)) {
             if (!stack.is(Items.CHEST)) {
                 graphics.text(mc.font, Component.translatable("chest_item.item.not_has"), centerX + 8, centerY - 4, Light.ARGB.color(255, 200, 20, 20));
@@ -168,15 +228,7 @@ public class CIBookScreen extends Screen {
             graphics.blit(RenderPipelines.GUI_TEXTURED,Identifier.fromNamespaceAndPath(Chestitem.MODID,"textures/item/star.png"),
                     centerX - 12, centerY - 12,0,0,16,16,16,16);
         }
-        if (mouseX >= centerX - 8 && mouseX <= centerX + 8 &&
-                mouseY >= centerY - 8 && mouseY <= centerY + 8) {
 
-            graphics.text(mc.font, ciBookGuiAdd.mainText, mouseX, mouseY, ciBookGuiAdd.colorMain);
-
-            for (int i = 0; i < ciBookGuiAdd.text.size(); i++) {
-                graphics.text(mc.font, ciBookGuiAdd.text.get(i), mouseX, mouseY + (i + 1) * 12, ciBookGuiAdd.colorText);
-            }
-        }
     }
     public boolean has(ItemStack stack){
         for (String string :player.getData(AttReg.itemRecord)) {
@@ -202,8 +254,9 @@ public class CIBookScreen extends Screen {
                         + offsetY
         );
 
-        if (mouseX >= centerX - 8 && mouseX <= centerX + 8 &&
-                mouseY >= centerY - 8 && mouseY <= centerY + 8) {
+        boolean b = mouseX >= centerX - 8 && mouseX <= centerX + 8 &&
+                mouseY >= centerY - 8 && mouseY <= centerY + 8;
+        if (b) {
 
             int paddingX = 4;
             int paddingY = 2;
@@ -231,11 +284,26 @@ public class CIBookScreen extends Screen {
                         Light.ARGB.color(200,0,0,0)
                 );
             }
-
-            graphics.text(mc.font, ciBookGuiAdd.mainText, mouseX, mouseY, ciBookGuiAdd.colorMain);
-
+            graphics.text(mc.font, ciBookGuiAdd.mainText, mouseX, mouseY,ciBookGuiAdd.colorMain);
             for (int i = 0; i < ciBookGuiAdd.text.size(); i++) {
                 graphics.text(mc.font, ciBookGuiAdd.text.get(i), mouseX, mouseY + (i + 1) * 12, ciBookGuiAdd.colorText);
+            }
+
+            ItemStack stack = new ItemStack(ciBookGuiAdd.item);
+            if (stack.getItem() instanceof SkillList element) {
+                Map<SkillBase, Identifier> IdentifierMap = element.name();
+                if (IdentifierMap != null) {
+                    for (int i = 0; i < IdentifierMap.keySet().stream().toList().size(); i++) {
+                        SkillBase elt = IdentifierMap.keySet().stream().toList().get(i);
+                        Identifier Identifier1 = IdentifierMap.get(elt);
+                        graphics.blitSprite(RenderPipelines.GUI_TEXTURED,Identifier1,mouseX - 48, mouseY + 32 * i,32,32);
+                        if (!(elt instanceof BlackSkill)) {
+                            graphics.blitSprite(RenderPipelines.GUI_TEXTURED, Identifier.fromNamespaceAndPath(Chestitem.MODID, "frame"), mouseX - 48, mouseY + 32 * i, 32, 32);
+                        } else {
+                            graphics.blitSprite(RenderPipelines.GUI_TEXTURED, Identifier.fromNamespaceAndPath(Chestitem.MODID, "frame_black"), mouseX - 48, mouseY + 32 * i, 32, 32);
+                        }
+                    }
+                }
             }
         }
     }
@@ -250,10 +318,11 @@ public class CIBookScreen extends Screen {
                     ),
                     Light.ARGB.color(255,255,255,255),
                     Light.ARGB.color(255,150,150,150),
-                    ThePage.BASE));
+                    ThePage.BASE,
+                    Light.ARGB.color(255,255,255,100)));
         }
     }
-    public record CIBookGuiAdd(Item item, Vec2 vecPos, Component mainText,List<Component> text, int colorMain,int colorText,ThePage thePage){}
+    public record CIBookGuiAdd(Item item, Vec2 vecPos, Component mainText,List<Component> text, int colorMain,int colorText,ThePage thePage,int lightColor){}
     public enum ThePage{
         BASE(Identifier.fromNamespaceAndPath(Chestitem.MODID,"textures/gui/book/base.png")),
         BLACK(Identifier.fromNamespaceAndPath(Chestitem.MODID,"textures/gui/book/black.png")),
